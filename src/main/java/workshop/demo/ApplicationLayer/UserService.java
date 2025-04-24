79ac615c573eca3fd2fb19c4ed3261fe50ff5b6b
bhaa225 <117650059+bhaa225@users.noreply.github.com>
2025-04-24 18:37:29 +0300

    adding service functions for login , register and generate guest

--- src/main/java/workshop/demo/ApplicationLayer/UserService.java
+++ src/main/java/workshop/demo/ApplicationLayer/UserService.java
@@ -1,5 +1,26 @@
 package workshop.demo.ApplicationLayer;
 
+import workshop.demo.DomainLayer.User.IUserRepo;
+
 public class UserService {
 
+    private IUserRepo userRepo;
+
+    public UserService(IUserRepo userRepo){
+        this.userRepo=userRepo;
+    }
+
+    public String generateGuest(){
+        return userRepo.generateGuest();
+    }
+
+    public void register(String token,String username,String password){
+        userRepo.registerUser(token, username, password);
+    }
+
+    public String login(String token , String username, String pass){
+        return userRepo.login(token, username, pass);
+    }
+
+    
 }
