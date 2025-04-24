72f7b483e696d736ef60484546d53fae71fe6d45
bhaa225 <117650059+bhaa225@users.noreply.github.com>
2025-04-24 10:47:01 +0300

    finish auth token , implementing the class on infrastructure

--- src/main/java/workshop/demo/InfrastructureLayer/AuthenticationRepo.java
+++ src/main/java/workshop/demo/InfrastructureLayer/AuthenticationRepo.java
@@ -5,23 +5,26 @@ import java.util.Date;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import workshop.demo.DomainLayer.Authentication.*;
+import workshop.demo.DomainLayer.Exceptions.TokenNotFoundException;
 
-public class AuthenticationRepo   {
+public class AuthenticationRepo  implements IAuthRepo {
     
 
     private final String SECRET = "secret_key_123"; // Use env/config in real apps
     private final long EXPIRATION_MS = 1000 * 60 * 60; // 1 hour
 
-    public String generateToken(String username) {
+    private String generateToken(String tokenValue) {
         return Jwts.builder()
-                .setSubject(username)
+                .setSubject(tokenValue)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MS))
                 .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public String extractUsername(String token) {
+    private String extractTokenValue(String token) {
+
         return Jwts.parserBuilder()
                 .setSigningKey(SECRET.getBytes())
                 .build()
@@ -30,7 +33,7 @@ public class AuthenticationRepo   {
                 .getSubject();
     }
 
-    public boolean validateToken(String token) {
+    private boolean validateToken(String token) {
         try {
             Jwts.parserBuilder()
                 .setSigningKey(SECRET.getBytes())
@@ -40,6 +43,41 @@ public class AuthenticationRepo   {
         } catch (Exception e) {
             return false;
         }
+    }
+
+    
+
+    @Override
+    public String getUserName(String token) {
+        if(!validateToken(token))
+            throw new TokenNotFoundException();
+        return new AuthoResponse(extractTokenValue(token)  ).userName;
+    }
+
+    @Override
+    public int getUserId(String token) {
+        if(!validateToken(token))
+            throw new TokenNotFoundException();
+        return new AuthoResponse(extractTokenValue(token)  ).id;
+    }
+
+    @Override
+    public boolean isRegistered(String token) {
+        if(!validateToken(token))
+            throw new TokenNotFoundException();
+        return new AuthoResponse(extractTokenValue(token)  ).isRegisterd();
+    }
+
+    @Override
+    public String generateGuestToken(int id) {
+        AuthoResponse req = new AuthoResponse(null,id);
+        return generateToken(req.toJson());
+    }
+
+    @Override
+    public String generateUserToken(int id, String username) {
+        AuthoResponse req = new AuthoResponse(username,id);
+        return generateToken(req.toJson());
     }
     
 }
