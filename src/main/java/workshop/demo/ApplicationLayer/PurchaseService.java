904bf43633cf3c0c30741f254211c8bb31e01a4a
bhaa225 <117650059+bhaa225@users.noreply.github.com>
2025-04-27 16:26:33 +0300

    add comments

--- src/main/java/workshop/demo/ApplicationLayer/PurchaseService.java
+++ src/main/java/workshop/demo/ApplicationLayer/PurchaseService.java
@@ -1,5 +1,21 @@
 package workshop.demo.ApplicationLayer;
 
+import workshop.demo.DTOs.RecieptDTO;
+
 public class PurchaseService {
+
+
+
+
+    public RecieptDTO buyGuestCart(String token){
+        //check validate token
+        //get id from token
+        //get shoping cart of guest
+        //check avalibality
+        //get sum 
+        //pay 
+        //supply 
+        //add order to order history
+    }
 
 }
