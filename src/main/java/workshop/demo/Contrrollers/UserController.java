d69b8523104595d9af5bf6e0746850f6d36aa9a3
bhaa225 <bhaah@post.bgu.ac.il>
2025-05-08 12:38:43 +0300

    adding controller without fixing compiling errors

--- src/main/java/workshop/demo/Contrrollers/UserController.java
+++ src/main/java/workshop/demo/Contrrollers/UserController.java
@@ -2,7 +2,6 @@ package workshop.demo.Contrrollers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import jakarta.servlet.http.HttpServletRequest;
 import workshop.demo.ApplicationLayer.Response;
 import workshop.demo.ApplicationLayer.UserService;
@@ -15,7 +14,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @Autowired // inject the service automatically
+    @Autowired
     public UserController(Repos repos) {
         this.userService = new UserService(repos.userRepo, repos.auth);
     }
@@ -23,20 +22,18 @@ public class UserController {
     @ModelAttribute
     public void beforeEveryRequest(HttpServletRequest request) {
         System.out.println("must check if the system get published by admin ...");
-        // You can do logging, auth checks, setup, etc.
-        //TODO by @bhaah
     }
 
     @GetMapping("/generateGuest")
     public String generateGuest() {
-        Response<String> res ;
+        Response<String> res;
         try {
             String token = userService.generateGuest();
-            res= new Response<String>(token, null);
+            res = new Response<>(token, null);
         } catch (UIException ex) {
-            res = new Response<String>(null, ex.getMessage(),ex.getNumber());
-        }catch(Exception e){
-            res = new Response<String>(null,e.getMessage(), -1);
+            res = new Response<>(null, ex.getMessage(), ex.getNumber());
+        } catch (Exception e) {
+            res = new Response<>(null, e.getMessage(), -1);
         }
         return res.toJson();
     }
@@ -45,106 +42,146 @@ public class UserController {
     public String register(@RequestParam String token,
                            @RequestParam String username,
                            @RequestParam String password) {
+        Response<String> res;
         try {
             userService.register(token, username, password);
-            return "done";
+            res = new Response<>("done", null);
+        } catch (UIException ex) {
+            res = new Response<>(null, ex.getMessage(), ex.getNumber());
         } catch (Exception e) {
-            return "error";
+            res = new Response<>(null, e.getMessage(), -1);
         }
+        return res.toJson();
     }
 
     @PostMapping("/login")
     public String login(@RequestParam String token,
                         @RequestParam String username,
                         @RequestParam String password) {
+        Response<String> res;
         try {
             userService.login(token, username, password);
-            return "done";
+            res = new Response<>("done", null);
+        } catch (UIException ex) {
+            res = new Response<>(null, ex.getMessage(), ex.getNumber());
         } catch (Exception e) {
-            return "error";
+            res = new Response<>(null, e.getMessage(), -1);
         }
+        return res.toJson();
     }
 
     @DeleteMapping("/destroyGuest")
     public String destroyGuest(@RequestParam String token) {
+        Response<String> res;
         try {
             userService.destroyGuest(token);
-            return "done";
+            res = new Response<>("done", null);
+        } catch (UIException ex) {
+            res = new Response<>(null, ex.getMessage(), ex.getNumber());
         } catch (Exception e) {
-            return "error";
+            res = new Response<>(null, e.getMessage(), -1);
         }
+        return res.toJson();
     }
 
     @PostMapping("/logout")
     public String logoutUser(@RequestParam String token) {
+        Response<String> res;
         try {
             userService.logoutUser(token);
-            return "done";
+            res = new Response<>("done", null);
+        } catch (UIException ex) {
+            res = new Response<>(null, ex.getMessage(), ex.getNumber());
         } catch (Exception e) {
-            return "error";
+            res = new Response<>(null, e.getMessage(), -1);
         }
+        return res.toJson();
     }
 
     @PostMapping("/setAdmin")
     public String setAdmin(@RequestParam String token,
                            @RequestParam String adminKey) {
+        Response<String> res;
         try {
             userService.setAdmin(token, adminKey);
-            return "done";
+            res = new Response<>("done", null);
+        } catch (UIException ex) {
+            res = new Response<>(null, ex.getMessage(), ex.getNumber());
         } catch (Exception e) {
-            return "error";
+            res = new Response<>(null, e.getMessage(), -1);
         }
+        return res.toJson();
     }
 
     @PostMapping("/addToCart")
     public String addToUserCart(@RequestParam String token,
                                 @RequestBody ItemStoreDTO itemToAdd) {
+        Response<String> res;
         try {
             userService.addToUserCart(token, itemToAdd);
-            return "done";
+            res = new Response<>("done", null);
+        } catch (UIException ex) {
+            res = new Response<>(null, ex.getMessage(), ex.getNumber());
         } catch (Exception e) {
-            return "error";
+            res = new Response<>(null, e.getMessage(), -1);
         }
+        return res.toJson();
     }
 
     @PutMapping("/updateProfile")
     public String updateProfile(@RequestParam String token) {
+        Response<String> res;
         try {
             userService.updateProfile(token);
-            return "done";
+            res = new Response<>("done", null);
+        } catch (UIException ex) {
+            res = new Response<>(null, ex.getMessage(), ex.getNumber());
         } catch (Exception e) {
-            return "error";
+            res = new Response<>(null, e.getMessage(), -1);
         }
+        return res.toJson();
     }
 
     @DeleteMapping("/removeUser")
     public String removeUser(@RequestParam String token,
                              @RequestParam String userToRemove) {
+        Response<String> res;
         try {
             userService.RemoveUser(token, userToRemove);
-            return "done";
+            res = new Response<>("done", null);
+        } catch (UIException ex) {
+            res = new Response<>(null, ex.getMessage(), ex.getNumber());
         } catch (Exception e) {
-            return "error";
+            res = new Response<>(null, e.getMessage(), -1);
         }
+        return res.toJson();
     }
 
     @GetMapping("/viewSystemPurchaseHistory")
     public String viewSystemPurchaseHistory(@RequestParam String token) {
+        Response<String> res;
         try {
             userService.ViewSystemPurchaseHistory(token);
-            return "done";
+            res = new Response<>("done", null);
+        } catch (UIException ex) {
+            res = new Response<>(null, ex.getMessage(), ex.getNumber());
         } catch (Exception e) {
-            return "error";
+            res = new Response<>(null, e.getMessage(), -1);
         }
+        return res.toJson();
     }
 
     @GetMapping("/viewSystemInfo")
     public String viewSystemInfo(@RequestParam String token) {
+        Response<String> res;
         try {
             userService.ViewSystemInfo(token);
-            return "done";
+            res = new Response<>("done", null);
+        } catch (UIException ex) {
+            res = new Response<>(null, ex.getMessage(), ex.getNumber());
         } catch (Exception e) {
-            return "error";
+            res = new Response<>(null, e.getMessage(), -1);
         }
+        return res.toJson();
     }
 }
