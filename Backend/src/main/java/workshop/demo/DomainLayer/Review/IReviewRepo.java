ad38821194920a45b91b91a9ce9b53a8fa754c42
RahafMrowat <mrowatr@post.bgu.ac.il>
2025-05-14 23:57:34 +0300

    fixing review

--- Backend/src/main/java/workshop/demo/DomainLayer/Review/IReviewRepo.java
+++ Backend/src/main/java/workshop/demo/DomainLayer/Review/IReviewRepo.java
@@ -2,16 +2,17 @@ package workshop.demo.DomainLayer.Review;
 
 import java.util.List;
 
-// import workshop.demo.DTOs.MessageDTO;
+import workshop.demo.DTOs.ReviewDTO;
 
+// import workshop.demo.DTOs.MessageDTO;
 public interface IReviewRepo {
 
-    public void AddReviewToProduct(int storeId, int productId, String review);
+    public void AddReviewToProduct(int storeId, int productId, int reviewerId, String name, String review);
 
-    public void AddReviewToStore(int storeId, String review);
+    public void AddReviewToStore(int storeId, int reviewerId, String name, String review);
 
     //must add gettt
-    public List<String> getReviewsForProduct(int storeId, int productId);
+    public List<ReviewDTO> getReviewsForProduct(int storeId, int productId);
 
-    public List<String> getReviewsForStore(int storeId);
+    public List<ReviewDTO> getReviewsForStore(int storeId);
 }
