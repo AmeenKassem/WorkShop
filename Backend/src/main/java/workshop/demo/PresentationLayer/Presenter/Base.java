e8ddb32eacac69d7e1495837cc841aebeed59135
m7mdghanem <ghanemmo@post.bgu.ac.il>
2025-06-11 21:47:24 +0300

    habibi ant wenak mn zman

--- Backend/src/main/java/workshop/demo/PresentationLayer/Presenter/Base.java
+++ Backend/src/main/java/workshop/demo/PresentationLayer/Presenter/Base.java
@@ -1,7 +1,7 @@
 package workshop.demo.PresentationLayer.Presenter;
 
 public class Base {
-    //public static String url = "http://10.0.0.6:8080";
+    public static String url = "http://10.0.0.6:8080";
 
-    public static String url = "http://127.0.0.1:8080";
+    // public static String url = "http://0000:8080";
 }
