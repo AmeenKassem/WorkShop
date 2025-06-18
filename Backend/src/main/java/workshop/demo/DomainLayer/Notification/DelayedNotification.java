822e40628546f53b73a810a88ac0b44498361499
RahafMrowat <mrowatr@post.bgu.ac.il>
2025-06-18 03:07:41 +0300

    notfication in db DONE!

---
+++ Backend/src/main/java/workshop/demo/DomainLayer/Notification/DelayedNotification.java
@@ -0,0 +1,54 @@
+package workshop.demo.DomainLayer.Notification;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class DelayedNotification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(length = 10000)
+    private String message;
+
+    public DelayedNotification() {
+    }
+
+    public DelayedNotification(String username, String message) {
+        this.username = username;
+        this.message = message;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
